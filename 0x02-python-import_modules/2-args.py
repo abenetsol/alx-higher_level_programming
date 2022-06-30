54fcdd85ea9bedd6ee407548673abb60fcb34d2a
abenet <solabenet@gmail.com>
2022-06-30 15:51:14 +0000

    task 2 jfix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     count = len(argv)
-    print("{:d} {:s}{:s}".format(count-1, "argument" if count <= 2 else "arguments", 
+    print("{:d} {:s}{:s}".format(count - 1, "argument" if count <= 2 else "arguments", 
                                  "." if count == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
