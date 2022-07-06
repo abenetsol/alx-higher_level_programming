3090641794b3c216f10dff0b7165d783ade0ed95
abenet <solabenet@gmail.com>
2022-07-06 17:38:26 +0000

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    res = []
-    res = 0
-    for item in my_list:
-        if item not in res:
-            res.append(item)
-    for unq in res:
-        res += uniqs
-    return res
+    return sum(set(my_list))
