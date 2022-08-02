59800eee8a3373c41dc520f3ce54fd85527ef927
abenet <solabenet@gmail.com>
2022-08-02 17:39:34 +0000

    task 7 fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Add item script."""
+"""Load, add, save"""
+
+
 import sys
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+filename = "add_item.json"
 try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
-
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+    _list = load_from_json_file(filename)
+except FileNotFoundError:
+    _list = []
+for i in range(1, len(sys.argv)):
+    _list.append(sys.argv[i])
+save_to_json_file(_list, filename)
