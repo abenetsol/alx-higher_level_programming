9519a564d0b837821722855a41e7013d54e6ea96
abenet <solabenet@gmail.com>
2022-08-02 16:28:22 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
-"""
-    7-add_item.py
-    Function that writes an Object to \
-    a text file, using a JSON representation.
-"""
-import sys
+"""Module for saving to json"""
 import json
+import os.path
+import sys
+from sys import argv
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+json_list = []
 
-def create_file_list():
-    """Adds all arguments to a Python list, and then save them to a file"""
-    obj_9 = load_from_json_file("add_item.json")
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
 
-    for i in range(1, len(sys.argv)):
-        obj_9.append(str(sys.argv[i]))
+for index in argv[1:]:
+    json_list.append(index)
 
-    save_to_json_file(obj_9, "add_item.json")
-
-if len(sys.argv) < 2:
-    try:
-        f = open("add_item.json")
-        f.close()
-    except IOError:
-        save_to_json_file([], "add_item.json")
-else:
-    try:
-        f = open("add_item.json")
-        f.close()
-        create_file_list()
-    except IOError:
-        save_to_json_file([], "add_item.json")
-        create_file_list()
+save_to_json_file(json_list, filename)
