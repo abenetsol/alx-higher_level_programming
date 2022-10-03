dd77b649cc24849ff90adaa10296ba7b3c192209
abenetsol <solabenet@gmail.com>
2022-10-03 19:29:23 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
+const argv = process.argv;
+const arr = [];
+if (argv.length < 4) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  for (let i = 2; i < argv.length; i++) {
+    arr[i - 2] = parseInt(argv[i]);
+  }
+  const max = Math.max.apply(null, arr);
+  arr.splice(arr.indexOf(max), 1);
+  const secondMax = Math.max.apply(null, arr);
+  console.log(secondMax);
 }
