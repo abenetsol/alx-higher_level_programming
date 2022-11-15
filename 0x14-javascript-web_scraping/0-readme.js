59a90967ee3bbb1b94f553f45306b25b82514820
abenetsol <solabenet@gmail.com>
2022-11-15 12:05:50 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // prints the content of a file
 const fs = require('fs');
-
 const filepath = process.argv[2];
 
 fs.readFile(filepath, 'utf-8', (error, data) => {
     if (error) {
 	console.error(error);
 	return;
-    }
+    }else{
     console.log(data);
+    }
 });
